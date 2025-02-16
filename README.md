02e89ef505f73f4423c267ab19aec63706812092
curso03 <tuemail@example.com>
2025-02-16 12:53:17 +0100

    proyecto creado

---
+++ README.md
@@ -0,0 +1,3 @@
+Ejercicio individual Bolígrafo Nunchy   ejerc003
+    Desarrollar un API REST para la clase BOLIGRAFO, con las propiedades: COLOR, SIESCRIBE y NOMBRE
+    Realizar operaciones CRUD.
---
+++ src/test/java/es/santander/ascender/ejerc003/Ejerc003ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.santander.ascender.ejerc003;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejerc003ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
